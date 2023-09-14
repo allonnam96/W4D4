a59b89e97f77338c90ce314f187ff51f0c4be679
“Eltion <ebehrami01@gmail.com>
2023-09-14 15:07:13 -0400

    anagrams done

--- practice-for-ch-ruby-anagrams-long-practice-main/Anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/Anagrams.rb
@@ -8,6 +8,32 @@ def first_anagram?(str1,str2)
 end
 
 def second_anagram?(str1, str2)
+
+    str1.each_char do |char|
+        str_2_array= str2.split("")
+        i = str_2_array.find_index(char) 
+        return false if i == nil
+        str2[i] = ""
+    end 
+        
+    str2 == ""
+
+end
+
+def third_anagram?(str1, str2)
+
+    word1 = str1.split('').sort
+    word2 = str2.split('').sort
+# debugger
+    word1 == word2
+end
+
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
+
+
+
+def fourth_anagram?(str1, str2)
     counter1 = Hash.new(0)
     counter2 = Hash.new(0)
 
@@ -18,15 +44,4 @@ def second_anagram?(str1, str2)
         counter2[char] += 1
     end
     counter1 == counter2
-end
-
-def third_anagram?(str1, str2)
-
-    word1 = str1.split('').sort
-    word2 = str2.split('').sort
-debugger
-    word1 == word2
-end
-
-p third_anagram?("gizmo", "sally")    #=> false
-p third_anagram?("elvis", "lives")    #=> true
+end 
