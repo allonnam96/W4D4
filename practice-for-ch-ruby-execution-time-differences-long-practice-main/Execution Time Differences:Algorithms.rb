209a45dadd0a3896c26668cd204d99453494aa7f
Allon Nam <allon.nam96@gmail.com>
2023-09-14 13:38:01 -0400

    my_min

---
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/Execution Time Differences:Algorithms.rb
@@ -0,0 +1,31 @@
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+
+def my_min(list)
+    min = list[0]
+    list.each do |ele1|
+        list.each do |ele2|
+    
+            next if ele1 == ele2
+            min = ele1 if ele1 < min
+            if ele1 > ele2
+                min = ele2
+
+            end
+        end
+    end
+    min
+end
+
+def my_min2(list)
+    smallest = list[0]
+        list.each do |ele|
+            if ele < smallest
+                smallest = ele
+            end
+        end
+        smallest
+end
+    
+p my_min(list)  # =>  -5
+p my_min2(list)  # =>  -5
+
